258df9b952dbb38bc23389f20eb4a88e8cf1ad0f
ManuelMolino <mmolmil158@g.educaand.es>
2025-02-05 13:47:49 +0100

    Inicio de ejercicios

---
+++ src/ejercicios/ejercicio1/Main.java
@@ -0,0 +1,39 @@
+package ejercicios.ejercicio1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws TrianguloException {
+    List<Triangulo> triangulos = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File("ficheros/triangulos.csv"))) {
+            scanner.nextLine();  //omito la cabecera
+            while (scanner.hasNextLine()) {
+                String[] tokens = scanner.nextLine().split(",");
+                try {
+                    Triangulo triangulo = Triangulo.getInstance(Integer.parseInt(tokens[0]),
+                            Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    System.out.printf("Leido triangulo l1: %s, l2: %s, l3: %s%n", tokens[0],
+                            tokens[1], tokens[2]);
+                    triangulos.add(triangulo);
+                }
+                catch (TrianguloException e) {
+                    System.err.print(e.getMessage());;
+                    System.err.println(" " + tokens[0] + "-" + tokens[1] + "-" + tokens[2]);;
+                }
+            }
+
+        } catch (FileNotFoundException  e) {
+            System.err.println(e.getMessage());;
+        }
+        System.out.printf("Nº de triángulos leídos: %d%n", triangulos.size());
+
+
+
+
+
+    }
+}
